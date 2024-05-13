1fb8764aa579dbae1df7be71b4d58a3aa4bccd9f
Mido <34779315+midobolt@users.noreply.github.com>
2024-05-13 19:01:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ function toggleLoginForm() {
   var loginForm = document.getElementById("loginForm");
   loginForm.classList.toggle("hidden");
 }
+
+// function showNavbar() {
+//    element = document.getElementById("navbar");
+//    element.innerHTML = "New Heading";
+// }
+
+document.addEventListener('scroll', () => {
+  const navbar = document.querySelector('nav');
+
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+})
