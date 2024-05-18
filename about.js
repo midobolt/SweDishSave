a7439e7b36e2f42940eb1fbe1d732da840f127ca
Mido <34779315+midobolt@users.noreply.github.com>
2024-05-18 18:26:23 +0200

    Add files via upload

--- about.js
+++ about.js
@@ -7,15 +7,15 @@ function growShrinkLogo() {
   if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
     Logo.style.height = "150px";
   } else {
-    Logo.style.height = "600px";
+    Logo.style.height = "100%";
   }
 
   if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
     document.getElementById("logo-img").style.height = "130px";
     document.getElementById("logo-img").style.width = "130px";
   } else {
-    document.getElementById("logo-img").style.height = "300px";
-    document.getElementById("logo-img").style.width = "300px";
+    document.getElementById("logo-img").style.height = "100%";
+    document.getElementById("logo-img").style.width = "100%";
   }
 }
 
